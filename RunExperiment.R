f1710f78d3b43b401e94eeb5c610d56389ea0e1e
LuchTiarna <pavelmysicka@seznam.cz>
2018-03-26 14:08:51 +0200

    Max Likelihood estimation

---
+++ RunExperiment.R
@@ -0,0 +1,7 @@
+#Runs all scripts of an experiments
+
+source('~/Documents/Bakalarka/FunRobScripts/initialization.R')
+source('~/Documents/Bakalarka/FunRobScripts/dataGeneration.R')
+source('~/Documents/Bakalarka/FunRobScripts/featureExtracting.R')
+source('~/Documents/Bakalarka/FunRobScripts/dataVisualisation.R')
+plot
